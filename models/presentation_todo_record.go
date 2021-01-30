f9f82a31e231e528c2b74df151b530cd01764aa0
irenicaa <root@irenica.pro>
2021-01-30 09:41:04 +0300

    Add the models.NewPresentationTodoRecord function

--- models/presentation_todo_record.go
+++ models/presentation_todo_record.go
@@ -1,9 +1,29 @@
 package models
 
+import (
+	"fmt"
+	"net/url"
+)
+
 // PresentationTodoRecord ...
 type PresentationTodoRecord struct {
 	URL       string
 	Title     string
 	Completed bool
 	Order     int
+}
+
+// NewPresentationTodoRecord ...
+func NewPresentationTodoRecord(
+	baseURL *url.URL,
+	todo TodoRecord,
+) PresentationTodoRecord {
+	url :=
+		fmt.Sprintf("%s://%s/api/v1/todos/%d", baseURL.Scheme, baseURL.Host, todo.ID)
+	return PresentationTodoRecord{
+		URL:       url,
+		Title:     todo.Title,
+		Completed: todo.Completed,
+		Order:     todo.Order,
+	}
 }
