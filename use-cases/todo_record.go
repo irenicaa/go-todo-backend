c4aedd2c962b61b4f3febf41d564ee4dfbc2df15
irenicaa <root@irenica.pro>
2021-02-02 23:00:20 +0300

    Add the usescases.TodoRecord.Create method

--- use-cases/todo_record.go
+++ use-cases/todo_record.go
@@ -1,10 +1,35 @@
 package usecases
 
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/irenicaa/go-todo-backend/models"
+)
+
 // Storage ...
 type Storage interface {
+	Create(todo models.TodoRecord) (id int, err error)
 }
 
 // TodoRecord ...
 type TodoRecord struct {
 	Storage Storage
+}
+
+// Create ...
+func (useCase TodoRecord) Create(baseURL *url.URL, todo models.TodoRecord) (
+	models.PresentationTodoRecord,
+	error,
+) {
+	id, err := useCase.Storage.Create(todo)
+	if err != nil {
+		return models.PresentationTodoRecord{},
+			fmt.Errorf("unable to create a to-do record: %v", err)
+	}
+
+	todo.ID = id
+
+	presentationTodo := models.NewPresentationTodoRecord(baseURL, todo)
+	return presentationTodo, nil
 }
