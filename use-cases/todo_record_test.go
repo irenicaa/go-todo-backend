3b9c3dcacfe6d05b7df089231c389743655f290d
irenicaa <root@irenica.pro>
2021-02-04 18:58:45 +0300

    Add tests for the usecases.TodoRecord.Update method

--- use-cases/todo_record_test.go
+++ use-cases/todo_record_test.go
@@ -102,3 +102,100 @@ func TestTodoRecord_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestTodoRecord_Update(t *testing.T) {
+	type fields struct {
+		Storage Storage
+	}
+	type args struct {
+		baseURL *url.URL
+		id      int
+		todo    models.TodoRecord
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    models.PresentationTodoRecord
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Storage: func() Storage {
+					todo := models.TodoRecord{
+						ID:        23,
+						Title:     "test",
+						Completed: true,
+						Order:     42,
+					}
+
+					storage := &MockStorage{}
+					storage.InnerMock.On("Update", 42, todo).Return(nil)
+
+					return storage
+				}(),
+			},
+			args: args{
+				baseURL: &url.URL{Scheme: "https", Host: "example.com"},
+				id:      42,
+				todo: models.TodoRecord{
+					ID:        23,
+					Title:     "test",
+					Completed: true,
+					Order:     42,
+				},
+			},
+			want: models.PresentationTodoRecord{
+				URL:       "https://example.com/api/v1/todos/42",
+				Title:     "test",
+				Completed: true,
+				Order:     42,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			fields: fields{
+				Storage: func() Storage {
+					todo := models.TodoRecord{
+						ID:        23,
+						Title:     "test",
+						Completed: true,
+						Order:     42,
+					}
+
+					storage := &MockStorage{}
+					storage.InnerMock.On("Update", 42, todo).Return(iotest.ErrTimeout)
+
+					return storage
+				}(),
+			},
+			args: args{
+				baseURL: &url.URL{Scheme: "https", Host: "example.com"},
+				id:      42,
+				todo: models.TodoRecord{
+					ID:        23,
+					Title:     "test",
+					Completed: true,
+					Order:     42,
+				},
+			},
+			want:    models.PresentationTodoRecord{},
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			useCase := TodoRecord{
+				Storage: tt.fields.Storage,
+			}
+			got, err := useCase.Update(tt.args.baseURL, tt.args.id, tt.args.todo)
+
+			tt.fields.Storage.(*MockStorage).InnerMock.AssertExpectations(t)
+			assert.Equal(t, tt.want, got)
+			tt.wantErr(t, err)
+		})
+	}
+}
