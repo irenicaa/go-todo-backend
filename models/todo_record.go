5ffefec7537a3ed030b2d6575c6b6a1fc5f5f120
irenicaa <root@irenica.pro>
2021-02-09 00:03:22 +0300

    Add the models.TodoRecord.Patch method

--- models/todo_record.go
+++ models/todo_record.go
@@ -7,3 +7,16 @@ type TodoRecord struct {
 	Completed bool
 	Order     int
 }
+
+// Patch ...
+func (todo *TodoRecord) Patch(patch TodoRecordPatch) {
+	if patch.Title != nil {
+		todo.Title = *patch.Title
+	}
+	if patch.Completed != nil {
+		todo.Completed = *patch.Completed
+	}
+	if patch.Order != nil {
+		todo.Order = *patch.Order
+	}
+}
