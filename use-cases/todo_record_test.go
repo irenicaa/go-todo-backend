24415638e7840e0c405de27a2ba5ef1b8bad64e8
irenicaa <root@irenica.pro>
2021-02-12 23:47:44 +0300

    Add tests for the usecases.TodoRecord.Patch method

--- use-cases/todo_record_test.go
+++ use-cases/todo_record_test.go
@@ -387,6 +387,142 @@ func TestTodoRecord_Update(t *testing.T) {
 	}
 }
 
+func TestTodoRecord_Patch(t *testing.T) {
+	type fields struct {
+		Storage Storage
+	}
+	type args struct {
+		baseURL   *url.URL
+		id        int
+		todoPatch models.TodoRecordPatch
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    models.PresentationTodoRecord
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Storage: func() Storage {
+					todo := models.TodoRecord{
+						ID:        23,
+						Title:     "test",
+						Completed: true,
+						Order:     42,
+					}
+					patchedTodo := models.TodoRecord{
+						ID:        23,
+						Title:     "test2",
+						Completed: true,
+						Order:     42,
+					}
+
+					storage := &MockStorage{}
+					storage.InnerMock.On("GetSingle", 23).Return(todo, nil)
+					storage.InnerMock.On("Update", 23, patchedTodo).Return(nil)
+
+					return storage
+				}(),
+			},
+			args: args{
+				baseURL: &url.URL{Scheme: "https", Host: "example.com"},
+				id:      23,
+				todoPatch: models.TodoRecordPatch{
+					Title: func() *string {
+						title := "test2"
+						return &title
+					}(),
+				},
+			},
+			want: models.PresentationTodoRecord{
+				URL:       "https://example.com/api/v1/todos/23",
+				Title:     "test2",
+				Completed: true,
+				Order:     42,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error with getting",
+			fields: fields{
+				Storage: func() Storage {
+					storage := &MockStorage{}
+					storage.InnerMock.
+						On("GetSingle", 23).
+						Return(models.TodoRecord{}, iotest.ErrTimeout)
+
+					return storage
+				}(),
+			},
+			args: args{
+				baseURL: &url.URL{Scheme: "https", Host: "example.com"},
+				id:      23,
+				todoPatch: models.TodoRecordPatch{
+					Title: func() *string {
+						title := "test2"
+						return &title
+					}(),
+				},
+			},
+			want:    models.PresentationTodoRecord{},
+			wantErr: assert.Error,
+		},
+		{
+			name: "error with updating",
+			fields: fields{
+				Storage: func() Storage {
+					todo := models.TodoRecord{
+						ID:        23,
+						Title:     "test",
+						Completed: true,
+						Order:     42,
+					}
+					patchedTodo := models.TodoRecord{
+						ID:        23,
+						Title:     "test2",
+						Completed: true,
+						Order:     42,
+					}
+
+					storage := &MockStorage{}
+					storage.InnerMock.On("GetSingle", 23).Return(todo, nil)
+					storage.InnerMock.On("Update", 23, patchedTodo).Return(iotest.ErrTimeout)
+
+					return storage
+				}(),
+			},
+			args: args{
+				baseURL: &url.URL{Scheme: "https", Host: "example.com"},
+				id:      23,
+				todoPatch: models.TodoRecordPatch{
+					Title: func() *string {
+						title := "test2"
+						return &title
+					}(),
+				},
+			},
+			want:    models.PresentationTodoRecord{},
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			useCase := TodoRecord{
+				Storage: tt.fields.Storage,
+			}
+			got, err := useCase.Patch(tt.args.baseURL, tt.args.id, tt.args.todoPatch)
+
+			tt.fields.Storage.(*MockStorage).InnerMock.AssertExpectations(t)
+			assert.Equal(t, tt.want, got)
+			tt.wantErr(t, err)
+		})
+	}
+}
+
 func TestTodoRecord_Delete(t *testing.T) {
 	type fields struct {
 		Storage Storage
