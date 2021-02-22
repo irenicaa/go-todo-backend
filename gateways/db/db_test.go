da79c5338acaa78e72ef8cc290c72e383b6f15e6
irenicaa <root@irenica.pro>
2021-02-22 23:21:59 +0300

    Add tests for the db.DB.Update method

--- gateways/db/db_test.go
+++ gateways/db/db_test.go
@@ -52,3 +52,49 @@ func TestDB_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestDB_Update(t *testing.T) {
+	type args struct {
+		originalTodo models.TodoRecord
+		updatedTodo  models.TodoRecord
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "success",
+			args: args{
+				originalTodo: models.TodoRecord{
+					Title:     "test",
+					Completed: true,
+					Order:     23,
+				},
+				updatedTodo: models.TodoRecord{
+					Title:     "test2",
+					Completed: false,
+					Order:     42,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, err := OpenDB(*dataSourceName)
+			require.NoError(t, err)
+
+			id, err := db.Create(tt.args.originalTodo)
+			require.NoError(t, err)
+
+			err = db.Update(id, tt.args.updatedTodo)
+			require.NoError(t, err)
+			tt.args.updatedTodo.ID = id
+
+			todo, err := db.GetSingle(id)
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.args.updatedTodo, todo)
+		})
+	}
+}
