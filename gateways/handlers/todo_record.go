224dff225af3364c50b71afedd561e8507a866f4
irenicaa <root@irenica.pro>
2021-02-26 23:44:35 +0300

    Add the handlers.TodoRecord.Create method

--- gateways/handlers/todo_record.go
+++ gateways/handlers/todo_record.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 
 	"github.com/irenicaa/go-todo-backend/models"
@@ -27,5 +30,48 @@ type TodoRecordUseCase interface {
 
 // TodoRecord ...
 type TodoRecord struct {
-	UseCase TodoRecordUseCase
+	URLScheme string
+	UseCase   TodoRecordUseCase
+}
+
+// Create ...
+func (handler TodoRecord) Create(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte(err.Error()))
+
+		return
+	}
+
+	var todo models.TodoRecord
+	if err := json.Unmarshal(body, &todo); err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte(err.Error()))
+
+		return
+	}
+
+	baseURL := &url.URL{Scheme: handler.URLScheme, Host: request.Host}
+	presentationTodo, err := handler.UseCase.Create(baseURL, todo)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(err.Error()))
+
+		return
+	}
+
+	response, err := json.Marshal(presentationTodo)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(err.Error()))
+
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(response)
 }
