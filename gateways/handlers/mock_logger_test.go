86bc691587c5eb9aa6ec77d26542fb64e9995bd5
irenicaa <root@irenica.pro>
2021-03-05 23:33:46 +0300

    Add tests for the handlers.TodoRecord.Create method

---
+++ gateways/handlers/mock_logger_test.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "github.com/stretchr/testify/mock"
+
+type MockLogger struct {
+	InnerMock mock.Mock
+}
+
+func (mock *MockLogger) Print(arguments ...interface{}) {
+	mock.InnerMock.Called(arguments)
+}
---
+++ gateways/handlers/mock_todo_record_use_case_test.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"net/url"
+
+	"github.com/irenicaa/go-todo-backend/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTodoRecordUseCase struct {
+	InnerMock mock.Mock
+}
+
+func (mock *MockTodoRecordUseCase) GetAll(
+	baseURL *url.URL,
+) ([]models.PresentationTodoRecord, error) {
+	results := mock.InnerMock.Called(baseURL)
+	return results.Get(0).([]models.PresentationTodoRecord), results.Error(1)
+}
+
+func (mock *MockTodoRecordUseCase) GetSingle(
+	baseURL *url.URL,
+	id int,
+) (models.PresentationTodoRecord, error) {
+	results := mock.InnerMock.Called(baseURL, id)
+	return results.Get(0).(models.PresentationTodoRecord), results.Error(1)
+}
+
+func (mock *MockTodoRecordUseCase) Create(
+	baseURL *url.URL,
+	todo models.TodoRecord,
+) (models.PresentationTodoRecord, error) {
+	results := mock.InnerMock.Called(baseURL, todo)
+	return results.Get(0).(models.PresentationTodoRecord), results.Error(1)
+}
+
+func (mock *MockTodoRecordUseCase) Update(
+	baseURL *url.URL,
+	id int,
+	todo models.TodoRecord,
+) (models.PresentationTodoRecord, error) {
+	results := mock.InnerMock.Called(baseURL, id, todo)
+	return results.Get(0).(models.PresentationTodoRecord), results.Error(1)
+}
+
+func (mock *MockTodoRecordUseCase) Patch(
+	baseURL *url.URL,
+	id int,
+	todoPatch models.TodoRecordPatch,
+) (models.PresentationTodoRecord, error) {
+	results := mock.InnerMock.Called(baseURL, id, todoPatch)
+	return results.Get(0).(models.PresentationTodoRecord), results.Error(1)
+}
+
+func (mock *MockTodoRecordUseCase) Delete(id int) error {
+	results := mock.InnerMock.Called(id)
+	return results.Error(0)
+}
