d6b9ca2edf0a6f040d1cf2d3f4c9dfd54d3b4073
irenicaa <root@irenica.pro>
2021-03-11 00:08:10 +0300

    Implement the handlers.Router.ServeHTTP method

--- gateways/handlers/router.go
+++ gateways/handlers/router.go
@@ -1,11 +1,17 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+
+	httputils "github.com/irenicaa/go-todo-backend/http-utils"
+)
 
 // Router ...
 type Router struct {
 	BaseURL    string
 	TodoRecord TodoRecord
+	Logger     httputils.Logger
 }
 
 // ServeHTTP ...
@@ -13,4 +19,31 @@ func (router Router) ServeHTTP(
 	writer http.ResponseWriter,
 	request *http.Request,
 ) {
+	if strings.HasPrefix(request.URL.Path, router.BaseURL+"/todos") {
+		switch request.Method {
+		case http.MethodPost:
+			router.TodoRecord.Create(writer, request)
+		case http.MethodGet:
+			if request.URL.Path == router.BaseURL+"/todos" {
+				router.TodoRecord.GetAll(writer, request)
+			} else {
+				router.TodoRecord.GetSingle(writer, request)
+			}
+		case http.MethodPut:
+			router.TodoRecord.Update(writer, request)
+		case http.MethodPatch:
+			router.TodoRecord.Patch(writer, request)
+		case http.MethodDelete:
+			router.TodoRecord.Delete(writer, request)
+		}
+
+		return
+	}
+
+	httputils.HandleError(
+		writer,
+		router.Logger,
+		http.StatusNotFound,
+		http.StatusText(http.StatusNotFound),
+	)
 }
