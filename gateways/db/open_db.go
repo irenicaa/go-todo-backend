fd67da994b8e37a65a8893b197c775fcd81c9f1a
irenicaa <root@irenica.pro>
2021-04-13 23:14:48 +0300

    Simplify the db.OpenDB function

--- gateways/db/open_db.go
+++ gateways/db/open_db.go
@@ -18,9 +18,5 @@ func OpenDB(dataSourceName string) (*sql.DB, error) {
 		return nil, fmt.Errorf("unable to create a pool of DB connections: %v", err)
 	}
 
-	if err := pool.Ping(); err != nil {
-		return nil, fmt.Errorf("unable to ping the DB: %v", err)
-	}
-
 	return pool, nil
 }
