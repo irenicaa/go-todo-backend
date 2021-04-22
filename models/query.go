7e872c763fb54bf29c7c676a1849168bc237a934
irenicaa <root@irenica.pro>
2021-04-22 23:28:31 +0300

    Complete the models.Query structure with the pagination parameters

--- models/query.go
+++ models/query.go
@@ -3,4 +3,11 @@ package models
 // Query ...
 type Query struct {
 	TitleFragment string
+	Pagination    Pagination
+}
+
+// Pagination ...
+type Pagination struct {
+	PageSize int
+	Page     int
 }
