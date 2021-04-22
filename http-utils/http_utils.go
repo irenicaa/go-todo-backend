915e4285b5fd651d5c11c5224c117b39e56acb4c
irenicaa <root@irenica.pro>
2021-04-22 23:27:04 +0300

    Add the httputils.GetIntFormValue function

--- http-utils/http_utils.go
+++ http-utils/http_utils.go
@@ -33,6 +33,32 @@ func GetIDFromURL(request *http.Request) (int, error) {
 	return id, nil
 }
 
+// GetIntFormValue ...
+func GetIntFormValue(
+	request *http.Request,
+	key string,
+	min int,
+	max int,
+) (int, error) {
+	value := request.FormValue(key)
+	if value == "" {
+		return 0, errors.New("key is missed")
+	}
+
+	valueAsInt, err := strconv.Atoi(value)
+	if err != nil {
+		return 0, fmt.Errorf("value is incorrect: %v", err)
+	}
+	if valueAsInt < min {
+		return 0, errors.New("value too less")
+	}
+	if valueAsInt > max {
+		return 0, errors.New("value too greater")
+	}
+
+	return valueAsInt, nil
+}
+
 // GetJSONData ...
 func GetJSONData(reader io.Reader, data interface{}) error {
 	dataAsJSON, err := ioutil.ReadAll(reader)
--- http-utils/http_utils_test.go
+++ http-utils/http_utils_test.go
@@ -72,6 +72,87 @@ func TestGetIDFromURL(t *testing.T) {
 	}
 }
 
+func TestGetIntFormValue(t *testing.T) {
+	type args struct {
+		request *http.Request
+		key     string
+		min     int
+		max     int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "/test?key=23", nil),
+				key:     "key",
+				min:     0,
+				max:     100,
+			},
+			want:    23,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error with a missed key",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "/test", nil),
+				key:     "key",
+				min:     0,
+				max:     100,
+			},
+			want:    0,
+			wantErr: assert.Error,
+		},
+		{
+			name: "error with an incorrect key",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "/test?key=value", nil),
+				key:     "key",
+				min:     0,
+				max:     100,
+			},
+			want:    0,
+			wantErr: assert.Error,
+		},
+		{
+			name: "error with a too less value",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "/test?key=23", nil),
+				key:     "key",
+				min:     50,
+				max:     100,
+			},
+			want:    0,
+			wantErr: assert.Error,
+		},
+		{
+			name: "error with a too greater value",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "/test?key=23", nil),
+				key:     "key",
+				min:     0,
+				max:     10,
+			},
+			want:    0,
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err :=
+				GetIntFormValue(tt.args.request, tt.args.key, tt.args.min, tt.args.max)
+
+			assert.Equal(t, tt.want, got)
+			tt.wantErr(t, err)
+		})
+	}
+}
+
 func TestGetJSONData(t *testing.T) {
 	type args struct {
 		reader io.Reader
