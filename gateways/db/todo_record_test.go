5beac7c7dce9db00161dd4324676de9cace5cd51
irenicaa <root@irenica.pro>
2021-07-05 18:28:26 +0300

    Simplify the tests of the db package

--- gateways/db/todo_record_test.go
+++ gateways/db/todo_record_test.go
@@ -5,6 +5,7 @@ package db
 import (
 	"database/sql"
 	"flag"
+	"strconv"
 	"testing"
 	"time"
 
@@ -19,131 +20,90 @@ var dataSourceName = flag.String(
 	"DB connection string",
 )
 
-func TestDB_GetAll(t *testing.T) {
-	type args struct {
-		todos []models.TodoRecord
+func TestDB_withGetting(t *testing.T) {
+	pool, err := OpenDB(*dataSourceName)
+	require.NoError(t, err)
+	db := NewTodoRecord(pool)
+
+	err = db.DeleteAll()
+	require.NoError(t, err)
+
+	var createdTodos []models.TodoRecord
+	for i := 0; i <= 10; i++ {
+		originalTodo := models.TodoRecord{
+			Date:      time.Date(2006, time.January, 2+i, 0, 0, 0, 0, time.UTC),
+			Title:     "test" + strconv.Itoa(i),
+			Completed: true,
+			Order:     i,
+		}
+
+		id, err2 := db.Create(originalTodo)
+		require.NoError(t, err2)
+		originalTodo.ID = id
+
+		createdTodos = append(createdTodos, originalTodo)
+	}
+	// reversing
+	for i, j := 0, len(createdTodos)-1; i < j; i, j = i+1, j-1 {
+		createdTodos[i], createdTodos[j] = createdTodos[j], createdTodos[i]
 	}
 
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "success",
-			args: args{
-				todos: []models.TodoRecord{
-					{
-						Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
-						Title:     "test",
-						Completed: true,
-						Order:     23,
-					},
-					{
-						Date:      time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC),
-						Title:     "test2",
-						Completed: false,
-						Order:     42,
-					},
-				},
-			},
-		},
+	gotTodos, err := db.GetAll(models.Query{})
+	require.NoError(t, err)
+	for index := range gotTodos {
+		gotTodos[index].Date = gotTodos[index].Date.In(time.UTC)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			pool, err := OpenDB(*dataSourceName)
-			require.NoError(t, err)
-			db := NewTodoRecord(pool)
 
-			err = db.DeleteAll()
-			require.NoError(t, err)
-
-			for index, todo := range tt.args.todos {
-				id, err2 := db.Create(todo)
-				require.NoError(t, err2)
-
-				tt.args.todos[index].ID = id
-			}
-			// reversing
-			for i, j := 0, len(tt.args.todos)-1; i < j; i, j = i+1, j-1 {
-				tt.args.todos[i], tt.args.todos[j] = tt.args.todos[j], tt.args.todos[i]
-			}
-
-			todos, err := db.GetAll(models.Query{})
-			require.NoError(t, err)
-			for index := range todos {
-				todos[index].Date = todos[index].Date.In(time.UTC)
-			}
-
-			assert.Equal(t, tt.args.todos, todos)
-		})
-	}
+	assert.Equal(t, createdTodos, gotTodos)
 }
 
-func TestDB_Create(t *testing.T) {
-	type args struct {
-		todo models.TodoRecord
-	}
-
+func TestDB_withModifying(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "success",
-			args: args{
-				todo: models.TodoRecord{
-					Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
-					Title:     "test",
-					Completed: true,
-					Order:     42,
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			pool, err := OpenDB(*dataSourceName)
-			require.NoError(t, err)
-			db := NewTodoRecord(pool)
-
-			id, err := db.Create(tt.args.todo)
-			require.NoError(t, err)
-			tt.args.todo.ID = id
-
-			todo, err := db.GetSingle(id)
-			require.NoError(t, err)
-			todo.Date = todo.Date.In(time.UTC)
-
-			assert.Equal(t, tt.args.todo, todo)
-		})
-	}
-}
-
-func TestDB_Update(t *testing.T) {
-	type args struct {
+		name         string
 		originalTodo models.TodoRecord
-		updatedTodo  models.TodoRecord
-	}
-
-	tests := []struct {
-		name string
-		args args
+		action       func(t *testing.T, db TodoRecord, todoID int)
+		wantTodo     models.TodoRecord
 	}{
 		{
-			name: "success",
-			args: args{
-				originalTodo: models.TodoRecord{
-					Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
-					Title:     "test",
-					Completed: true,
-					Order:     23,
-				},
-				updatedTodo: models.TodoRecord{
+			name: "creation",
+			originalTodo: models.TodoRecord{
+				Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
+				Title:     "test",
+				Completed: true,
+				Order:     23,
+			},
+			action: func(t *testing.T, db TodoRecord, todoID int) {},
+			wantTodo: models.TodoRecord{
+				Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
+				Title:     "test",
+				Completed: true,
+				Order:     23,
+			},
+		},
+		{
+			name: "updating",
+			originalTodo: models.TodoRecord{
+				Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
+				Title:     "test",
+				Completed: true,
+				Order:     23,
+			},
+			action: func(t *testing.T, db TodoRecord, todoID int) {
+				newTodo := models.TodoRecord{
 					Date:      time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC),
 					Title:     "test2",
 					Completed: false,
 					Order:     42,
-				},
+				}
+
+				err := db.Update(todoID, newTodo)
+				require.NoError(t, err)
+			},
+			wantTodo: models.TodoRecord{
+				Date:      time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC),
+				Title:     "test2",
+				Completed: false,
+				Order:     42,
 			},
 		},
 	}
@@ -153,112 +113,41 @@ func TestDB_Update(t *testing.T) {
 			require.NoError(t, err)
 			db := NewTodoRecord(pool)
 
-			id, err := db.Create(tt.args.originalTodo)
+			id, err := db.Create(tt.originalTodo)
 			require.NoError(t, err)
 
-			err = db.Update(id, tt.args.updatedTodo)
+			tt.action(t, db, id)
+
+			gotTodo, err := db.GetSingle(id)
 			require.NoError(t, err)
-			tt.args.updatedTodo.ID = id
+			gotTodo.Date = gotTodo.Date.In(time.UTC)
 
-			todo, err := db.GetSingle(id)
-			require.NoError(t, err)
-			todo.Date = todo.Date.In(time.UTC)
-
-			assert.Equal(t, tt.args.updatedTodo, todo)
-		})
-	}
-}
-
-func TestDB_DeleteAll(t *testing.T) {
-	type args struct {
-		todos []models.TodoRecord
-	}
-
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "success",
-			args: args{
-				todos: []models.TodoRecord{
-					{
-						Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
-						Title:     "test",
-						Completed: true,
-						Order:     23,
-					},
-					{
-						Date:      time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC),
-						Title:     "test2",
-						Completed: false,
-						Order:     42,
-					},
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			pool, err := OpenDB(*dataSourceName)
-			require.NoError(t, err)
-			db := NewTodoRecord(pool)
-
-			for _, todo := range tt.args.todos {
-				_, err2 := db.Create(todo)
-				require.NoError(t, err2)
-			}
-
-			err = db.DeleteAll()
-			require.NoError(t, err)
-
-			todos, err := db.GetAll(models.Query{})
-			require.NoError(t, err)
-
-			assert.Empty(t, todos)
+			tt.wantTodo.ID = id
+			assert.Equal(t, tt.wantTodo, gotTodo)
 		})
 	}
 }
 
 func TestDB_DeleteSingle(t *testing.T) {
-	type args struct {
-		todo models.TodoRecord
+	pool, err := OpenDB(*dataSourceName)
+	require.NoError(t, err)
+	db := NewTodoRecord(pool)
+
+	originalTodo := models.TodoRecord{
+		Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
+		Title:     "test",
+		Completed: true,
+		Order:     23,
 	}
 
-	tests := []struct {
-		name    string
-		args    args
-		wantErr error
-	}{
-		{
-			name: "success",
-			args: args{
-				todo: models.TodoRecord{
-					Date:      time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC),
-					Title:     "test",
-					Completed: true,
-					Order:     42,
-				},
-			},
-			wantErr: sql.ErrNoRows,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			pool, err := OpenDB(*dataSourceName)
-			require.NoError(t, err)
-			db := NewTodoRecord(pool)
+	id, err := db.Create(originalTodo)
+	require.NoError(t, err)
 
-			id, err := db.Create(tt.args.todo)
-			require.NoError(t, err)
+	err = db.DeleteSingle(id)
+	require.NoError(t, err)
 
-			err = db.DeleteSingle(id)
-			require.NoError(t, err)
+	todo, err := db.GetSingle(id)
 
-			todo, err := db.GetSingle(id)
-
-			assert.Equal(t, models.TodoRecord{}, todo)
-			assert.Equal(t, tt.wantErr, err)
-		})
-	}
+	assert.Equal(t, models.TodoRecord{}, todo)
+	assert.Equal(t, sql.ErrNoRows, err)
 }
